3ac7e6942ad62dcbcd710690b8904c9d21d57c2f
Arnab Deka <arnab.deka@gmail.com>
2012-12-23 20:28:39 +0530

    Added about activity

--- src/org/example/sudoku/SudokuActivity.java
+++ src/org/example/sudoku/SudokuActivity.java
@@ -2,14 +2,30 @@ package org.example.sudoku;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class SudokuActivity extends Activity {
+public class SudokuActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sudoku);
+		
+		
+		View continueButton = findViewById(R.id.continue_button);
+		continueButton.setOnClickListener(this);
+		
+		View newGameButton = findViewById(R.id.new_game_button);
+		newGameButton.setOnClickListener(this);
+		
+		View aboutButton = findViewById(R.id.about_button);
+		aboutButton.setOnClickListener(this);
+
+		View exitButton = findViewById(R.id.exit_button);
+		exitButton.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +33,16 @@ public class SudokuActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_sudoku, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()) {
+		case R.id.about_button:
+			Intent i = new Intent(this, AboutActivity.class);
+			startActivity(i);
+			break;
+		}
 	}
 
 }
