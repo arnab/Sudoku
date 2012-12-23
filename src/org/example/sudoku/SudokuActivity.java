89ed16bcc76987f1b71b2ab270212c54e0e04788
Arnab Deka <arnab.deka@gmail.com>
2012-12-23 23:57:06 +0530

    Settings menu!
    Settings themselves don't work. Yet.

--- src/org/example/sudoku/SudokuActivity.java
+++ src/org/example/sudoku/SudokuActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 
@@ -31,10 +32,21 @@ public class SudokuActivity extends Activity implements OnClickListener {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
+		super.onCreateOptionsMenu(menu);
 		getMenuInflater().inflate(R.menu.activity_sudoku, menu);
 		return true;
 	}
 
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+		case R.id.menu_settings:
+			startActivity(new Intent(this, Prefs.class));
+		}
+		return false;
+	}
+	
 	@Override
 	public void onClick(View v) {
 		switch(v.getId()) {
@@ -44,5 +56,4 @@ public class SudokuActivity extends Activity implements OnClickListener {
 			break;
 		}
 	}
-
 }
